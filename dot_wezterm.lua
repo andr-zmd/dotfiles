e6a1014f912975103550b0034a1930bdc7fc24b4
andr-zmd <zamudioandre2506@gmail.com>
2025-08-30 13:37:05 -0400

    added os based configurations

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,13 +1,32 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
+local function getOS()
+	local pathSeparator = package.config:sub(1, 1)
+	if pathSeparator == "\\" then
+		return "Windows"
+	else
+		return "Unix"
+	end
+end
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- Terminal
-config.enable_wayland = false
+--
+
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.7
+
+-- OS Specific Configs
+if getOS() == "Windows" then
+	-- Window Configs
+	config.default_prog = { "powershell.exe" }
+	config.window_decorations = "RESIZE"
+else
+	-- Linux Configs
+	config.enable_wayland = false
+end
 
 -- Finally, return the configuration to wezterm:
 return config
