e3d22fc75ff820e3af8a0cced35ae9df0bba9dbf
andr-zmd <zamudioandre2506@gmail.com>
2025-09-24 01:31:25 -0400

    updated jdtls config

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
 return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		{ "mason-org/mason-lspconfig.nvim", opts = {} },
+		{ "mason-org/mason-lspconfig.nvim", opts = { automatic_enable = { exclude = { "jdtls" } } } },
 	},
 }
