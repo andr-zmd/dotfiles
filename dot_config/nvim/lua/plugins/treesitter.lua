93200b4d4293dfe97409fa491df5063437facdfd
andr-zmd <zamudioandre2506@gmail.com>
2026-03-06 21:22:42 -0500

    changed hypr, nvim, and waybar configs

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,15 +2,4 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
 	build = ":TSUpdate",
-	config = function()
-		local parsers =
-			{ "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc" }
-		require("nvim-treesitter").install(parsers)
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = parsers,
-			callback = function()
-				vim.treesitter.start()
-			end,
-		})
-	end,
 }
